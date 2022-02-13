9564667598fc6df8a9d1166713e7b799f14471ad
dhivainfo084@gmail.com <dhivainfo084@gmail.com>
2022-02-13 23:04:34 +0530

    modified required constants
    Signed-off-by: dhivainfo084@gmail.com <dhivainfo084@gmail.com>

--- src/main/java/com/dhivakar/jiosaavnsongdownloader/constants/UrlConstants.java
+++ src/main/java/com/dhivakar/jiosaavnsongdownloader/constants/UrlConstants.java
@@ -2,10 +2,32 @@ package com.dhivakar.jiosaavnsongdownloader.constants;
 
 public class UrlConstants {
 
-    private static final String ALBUMFROMID = "https://www.jiosaavn.com/api.php?__call=content.getAlbumDetails&_format=json&cc=in&_marker=0%3F_marker=0&albumid=${id}";
-    private static final String ALBUMSEARCHFROMSTRING = "https://www.jiosaavn.com/api.php?__call=autocomplete.get&_format=json&_marker=0&cc=in&includeMetaTags=1&query=${query}";
-    private static final String SONGFROMID = "https://www.jiosaavn.com/api.php?__call=autocomplete.get&_format=json&_marker=0&cc=in&includeMetaTags=1&query=${query}";
-    private static final String SONGSEARCHFROMSTRING = "https://www.jiosaavn.com/api.php?p=1&_format=json&_marker=0&api_version=4&ctx=wap6dot0&n=10&__call=search.getResults&q=${query}";
-    private static final String LYRICSFROMID = "https://www.jiosaavn.com/api.php?__call=lyrics.getLyrics&ctx=web6dot0&api_version=4&_format=json&_marker=0%3F_marker=0&lyrics_id=${id}";
 
+    public static final String ID = "${id}";
+    public static final String QUERY = "${query}";
+    private final String ALBUMFROMID = "https://www.jiosaavn.com/api.php?__call=content.getAlbumDetails&_format=json&cc=in&_marker=0%3F_marker=0&albumid=${id}";
+    private final String ALBUMSEARCHFROMSTRING = "https://www.jiosaavn.com/api.php?__call=autocomplete.get&_format=json&_marker=0&cc=in&includeMetaTags=1&query=${query}";
+    private final String SONGFROMID = "https://www.jiosaavn.com/api.php?__call=autocomplete.get&_format=json&_marker=0&cc=in&includeMetaTags=1&query=${query}";
+    private final String SONGSEARCHFROMSTRING = "https://www.jiosaavn.com/api.php?p=1&_format=json&_marker=0&api_version=4&ctx=wap6dot0&n=10&__call=search.getResults&q=${query}";
+    private final String LYRICSFROMID = "https://www.jiosaavn.com/api.php?__call=lyrics.getLyrics&ctx=web6dot0&api_version=4&_format=json&_marker=0%3F_marker=0&lyrics_id=${id}";
+
+    public String getAlbumFromID(String id) {
+        return ALBUMFROMID.replace(ID, id);
+    }
+
+    public String getAlbumSearchFromSTRING(String query) {
+        return ALBUMSEARCHFROMSTRING.replace(QUERY, String.join("+", query.split(" ")));
+    }
+
+    public String getSongFromID(String id) {
+        return SONGFROMID.replace(ID, id);
+    }
+
+    public String getSongSearchFromSTRING(String query) {
+        return SONGSEARCHFROMSTRING.replace(QUERY, String.join("+", query.split(" ")));
+    }
+
+    public String getLyricsFromID(String id) {
+        return LYRICSFROMID.replace(ID, id);
+    }
 }
