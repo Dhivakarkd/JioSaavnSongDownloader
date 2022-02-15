60f1192d38f9399995e4edba1f06b7797918ae32
dhivainfo084@gmail.com <dhivainfo084@gmail.com>
2022-02-15 19:38:04 +0530

    added additional helper functions
    Signed-off-by: dhivainfo084@gmail.com <dhivainfo084@gmail.com>

--- src/main/java/com/dhivakar/jiosaavnsongdownloader/Main.java
+++ src/main/java/com/dhivakar/jiosaavnsongdownloader/Main.java
@@ -1,8 +1,33 @@
 package com.dhivakar.jiosaavnsongdownloader;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.util.Arrays;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+
+        String link = "https://www.jiosaavn.com/song/arabic-kuthu-from-beast/PhEOejlpQXE";
+
+
+        String id = extractIdFromLink(link);
+
+
+
+
+        
+    }
+
+    private static String extractIdFromLink(String link) throws IOException {
+        Document doc = Jsoup.connect(link).get();
+
+
+        String id = doc.html().split("\"song\":\\{\"type\":\"")[1].split(",\"image\":")[0]
+                .split("\"")[8];
+        return id;
     }
 }
