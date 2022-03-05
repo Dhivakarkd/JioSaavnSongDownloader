0666b4d49815b37c40aaf70e93479ad42c8bc1cb
dhivainfo084@gmail.com <dhivainfo084@gmail.com>
2022-03-05 17:12:27 +0530

    added File Convertor from Mp4 to Mp3 Logic
    Signed-off-by: dhivainfo084@gmail.com <dhivainfo084@gmail.com>

--- src/main/java/com/dhivakar/jiosaavnsongdownloader/helper/AudioHelper.java
+++ src/main/java/com/dhivakar/jiosaavnsongdownloader/helper/AudioHelper.java
@@ -2,6 +2,10 @@ package com.dhivakar.jiosaavnsongdownloader.helper;
 
 import com.dhivakar.jiosaavnsongdownloader.model.SongModel;
 import lombok.extern.slf4j.Slf4j;
+import net.bramp.ffmpeg.FFmpeg;
+import net.bramp.ffmpeg.FFmpegExecutor;
+import net.bramp.ffmpeg.FFprobe;
+import net.bramp.ffmpeg.builder.FFmpegBuilder;
 import org.jaudiotagger.audio.AudioFile;
 import org.jaudiotagger.audio.AudioFileIO;
 import org.jaudiotagger.audio.exceptions.CannotReadException;
@@ -13,8 +17,10 @@ import org.jaudiotagger.tag.Tag;
 import org.jaudiotagger.tag.TagException;
 import org.jaudiotagger.tag.datatype.Artwork;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 @Slf4j
 public class AudioHelper {
@@ -36,4 +42,52 @@ public class AudioHelper {
 
         log.info("Audio MetaData Written to file Successfully");
     }
+
+
+    public static void convertFileToMp3(String fileName) {
+
+        ProcessBuilder processBuilder = new ProcessBuilder();
+
+        String convertionCommandTemplate = "/usr/bin/ffmpeg -i $fileName.mp4 -b:a 320K -vn $fileName.mp3";
+
+        String convertionCommand = convertionCommandTemplate.replace("$fileName",fileName);
+
+
+        // Run a shell command
+        processBuilder.command("bash", "-c", convertionCommand);
+        processBuilder.directory(new File("TempDir"));
+
+
+        try {
+
+            Process process = processBuilder.start();
+
+            StringBuilder output = new StringBuilder();
+
+
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(process.getInputStream()));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line).append("\n");
+            }
+
+            int exitVal = process.waitFor();
+            if (exitVal == 0) {
+                System.out.println("Success!");
+                System.out.println(output);
+
+            } else {
+                //abnormal...
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
